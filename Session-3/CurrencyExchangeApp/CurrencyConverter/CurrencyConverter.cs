51e18a1831215bb76f7ed4e959ddff447720512c
prashanthyarram <prashanthyarram@gmail.com>
2023-01-09 18:18:07 +0530

    Session 4 changes resolved

---
+++ Session-3/CurrencyExchangeApp/CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+
+
+namespace CurrencyExchange
+{
+    public class CurrencyConverter
+    {
+        public static double Convert(string fromCurrency, string toCurrency, double currencyValue)
+        {
+            string currencyStamp = $"{toCurrency}{fromCurrency}";
+            var client = new RestClient($"https://api.apilayer.com/currency_data/live?source={toCurrency}&currencies={fromCurrency}");
+            var request = new RestRequest();
+            request.AddHeader("apikey", "NKJlyNE9umLraoNvP87N7W9v6NNePbI8");
+            ApiResponse apiObject;
+            RestResponse response = client.Get(request);
+            apiObject = JsonConvert.DeserializeObject<ApiResponse>(response.Content);
+            if (apiObject.success==true)
+            {
+                if(toCurrency == fromCurrency)
+                {
+                    return 1 * currencyValue;
+                }
+                double curr_val = apiObject.quotes[currencyStamp];
+                return curr_val * currencyValue;
+            }
+            else if (apiObject.success == false)
+            {
+                CurrrencyNotFoundException notFoundException = new CurrrencyNotFoundException("Enter a valid Currency[Ex : USD, INR]",currencyStamp);
+                throw notFoundException;
+            }
+            return 0.00;
+        }
+    }
+}
+
---
+++ Session-3/CurrencyExchangeApp/CurrencyConverter/CurrencyNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CurrencyExchange
+{
+    public class CurrrencyNotFoundException : Exception
+    {
+        public CurrrencyNotFoundException()
+        {
+        }
+        public string CurrencyCode { get; set; }
+        public CurrrencyNotFoundException(string message, string currencyCode)
+          : base(message)
+        {
+            this.CurrencyCode = currencyCode;
+        }
+    }
+}
