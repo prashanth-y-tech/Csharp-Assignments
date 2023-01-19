6df5924076d862b31e8ddccc3810e8fcc60da747
prashanthyarram <prashanth.y@technovert.com>
2023-01-19 11:50:00 +0530

    Changes resolved after review 4

---
+++ Session-5/CurrencyExchangeApplication/ExtractCurrencyDB/RecordNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CurrencyExchange
+{
+    public class RecordNotFoundException : Exception
+    {
+        public string RecordKey { get; set; }
+        public RecordNotFoundException()
+        {
+        }
+        public RecordNotFoundException(string message, string recordKey) : base(message)
+        {
+            this.RecordKey = recordKey;
+        }
+    }
+}
